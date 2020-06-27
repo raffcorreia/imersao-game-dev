fef1b4dc9c44c26d5795c493ea886d3fcd81d1c3
Rafael Correia <raffcorreia@gmail.com>
2020-06-27 00:51:54 -0400

    refactor code adding script files for variables and load

---
+++ scripts/load.js
@@ -0,0 +1,9 @@
+function preload() {
+    imagemCenario = loadImage('imagens/cenario/floresta.png')
+    imagemGameOver = loadImage('imagens/assets/game-over.png')
+    imagemPersonagem = loadImage('imagens/personagem/correndo.png')
+    imagemInimigo = loadImage('imagens/inimigos/gotinha.png')
+    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png')
+    imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png')
+    somDoJogo = new loadSound('sons/trilha_jogo.mp3');
+}
--- sketch.js
+++ sketch.js
@@ -1,134 +1,3 @@
-let imagemCenario;
-let imagemGameOver;
-let imagemPersonagem;
-let imagemInimigo;
-let imagemInimigoGrande;
-let imagemInimigoVoador;
-
-let cenario;
-let pontuacao;
-let personagem;
-let inimigo;
-let inimigoGrande;
-let inimigoVoador;
-
-let somDoJogo;
-
-let somEstaTocando = false;
-let inimigoAtual = 0;
-
-const matrizInimigo = [
-        [0, 0],
-        [104, 0],
-        [208, 0],
-        [312, 0],
-        [0, 104],
-        [104, 104],
-        [208, 104],
-        [312, 104],
-        [0, 208],
-        [104, 208],
-        [208, 208],
-        [312, 208],
-        [0, 312],
-        [104, 312],
-        [208, 312],
-        [312, 312],
-        [0, 416],
-        [104, 416],
-        [208, 416],
-        [312, 416],
-        [0, 520],
-        [104, 520],
-        [208, 520],
-        [312, 520],
-        [0, 624],
-        [104, 624],
-        [208, 624],
-        [312, 624],
-    ];
-
-const matrizHipsta =[
-        [0, 0],
-        [220, 0],
-        [440, 0],
-        [660, 0],
-        [0, 270],
-        [220, 270],
-        [440, 270],
-        [660, 270],
-        [0, 540],
-        [220, 540],
-        [440, 540],
-        [660, 540],
-        [0, 810],
-        [220, 810],
-        [440, 810],
-        [660, 810],
-    ];
-    
-    const matrizInimigoGrande = [
-        [0,0],
-        [400,0],
-        [800,0],
-        [1200,0],
-        [1600,0],
-        [0,400],
-        [400,400],
-        [800,400],
-        [1200, 400],
-        [1600, 400],
-        [0,800],
-        [400, 800],
-        [800, 800],
-        [1200, 800],
-        [1600, 800],
-        [0, 1200],
-        [400, 1200],
-        [800, 1200],
-        [1200, 1200],
-        [1600, 1200], 
-        [0, 1600],
-        [400, 1600],
-        [800, 1600],
-        [1200, 1600],
-        [1600, 1600],
-        [0, 2000],
-        [400, 2000],
-        [800, 2000],
-      ];
-
-      const matrizInimigoVoador = [
-        [0,0],
-        [200, 0],
-        [400, 0],
-        [0, 150],
-        [200, 150],
-        [400, 150],
-        [0, 300],
-        [200, 300],
-        [400, 300],
-        [0, 450],
-        [200, 450],
-        [400, 450],
-        [0, 600],
-        [200, 600],
-        [400, 600],
-        [0, 750],
-      ];
-
-const inimigos = [];
-
-function preload() {
-    imagemCenario = loadImage('imagens/cenario/floresta.png')
-    imagemGameOver = loadImage('imagens/assets/game-over.png')
-    imagemPersonagem = loadImage('imagens/personagem/correndo.png')
-    imagemInimigo = loadImage('imagens/inimigos/gotinha.png')
-    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png')
-    imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png')
-    somDoJogo = new loadSound('sons/trilha_jogo.mp3');
-}
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
     cenario = new Cenario(imagemCenario, 3);
@@ -137,8 +6,8 @@ function setup() {
     personagem = new Personagem(matrizHipsta, imagemPersonagem, 0, 30, 110, 135, 220, 270);
     
     const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10, 100);
-    const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width, 30, 200, 200, 400, 400, 10, 2000);
-    const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 200, 100, 75, 200, 150, 10, 2000);
+    const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width, 30, 200, 200, 400, 400, 15, 100);
+    const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 200, 100, 75, 200, 150, 10, 100);
     // frameRate(40);
 
     inimigos.push(inimigo);
@@ -171,7 +40,7 @@ function draw() {
     
     if(inimigoSaiu) {
         pontuacao.adcionarPonto();
-        
+
         inimigoAtual++;
         
         inimigo.limpa();
@@ -179,6 +48,8 @@ function draw() {
         if(inimigoAtual >= inimigos.length) {
             inimigoAtual = 0;
         }
+        inimigo.velocidae = parseInt(random(10, 30));
+        console.log(inimigo.velocidae);
     }
 
     if(personagem.estaColidindo(inimigo)){
